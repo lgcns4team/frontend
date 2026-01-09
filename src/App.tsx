0e8cb35e51358400212c5616197052a4c20e3497
YimJeongMin름 <soft1049@gmail.com>
2026-01-09 14:50:11 +0900

    커서및 아이콘 적용

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import VoiceOrder from './pages/VoiceOrder';
 import Advertisement from './pages/Advertisement';
 import './index.css';
 import { useIdleWatcher } from './hooks/useIdleWatcher';
+import CustomCursor from './components/CustomCursor';
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+    <CustomCursor />
+    <AppRoutes />
     </BrowserRouter>
   );
 }
