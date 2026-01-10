1df7f9e3024f666193de81ab5df151f1ccaa03f4
김현우 <rlagusdn1210@gmail.com>
2026-01-10 16:02:35 +0900

    배포 주소 변경

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 //환경 변수에 따라 주소가 자동으로 바뀝니다.
 export const apiClient = axios.create({
   // import.meta.env.VITE_API_URL : Vite가 .env 파일에서 값을 가져오는 문법입니다.
-  baseURL: import.meta.env.VITE_API_URL || '/api', 
-  // baseURL: 'https://api.bfree-kiosk.com/nok-nok/api',
+  // baseURL: import.meta.env.VITE_API_URL || '/api', 
+  baseURL: 'https://api.bfree-kiosk.com/nok-nok/api',
   headers: {
     'Content-Type': 'application/json',
   },
