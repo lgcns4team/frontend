f73f4de49e05d3ceca0dd4be8c0717e3d6b47aaa
김현우 <rlagusdn1210@gmail.com>
2026-01-16 13:47:30 +0900

    local test

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -5,7 +5,7 @@ export const apiClient = axios.create({
   // import.meta.env.VITE_API_URL : Vite가 .env 파일에서 값을 가져오는 문법입니다.
   // baseURL: import.meta.env.VITE_API_URL || '/api',
   // baseURL: 'https://api.bfree-kiosk.com/nok-nok/api',
-  baseURL: '/api',
+  baseURL: 'http://localhost:8080/nok-nok/api',
 
   headers: {
     'Content-Type': 'application/json',
